66c9f5118afdba5162611a9ce54abf5c2102df60
Jordi Rocha <jordirocharocha@gmail.com>
2024-10-09 21:51:18 +0200

    exam exercise

--- Jutge/PRO1 2024-2025 Q1 D3_T1_online/almenys_5_nombres_abca.cpp
+++ Jutge/PRO1 2024-2025 Q1 D3_T1_online/almenys_5_nombres_abca.cpp
@@ -4,4 +4,37 @@ using namespace std;
 
 int main()
 {
+    int n;
+    int counterNumbers = 0;
+
+    while (cin >> n)
+    {
+        int copyN = n;
+        int counter = 0;
+        int firstDigit = 0;
+
+        while (n > 0)
+        {
+            if (n < 10)
+                firstDigit = n;
+            n /= 10;
+            counter++;
+        }
+
+        if (counter > 3)
+        {
+            int lastDigit = copyN % 10;
+            if (firstDigit == lastDigit)
+                counterNumbers++;
+        }
+    }
+
+    if (counterNumbers >= 5)
+    {
+        cout << "SI" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
 }

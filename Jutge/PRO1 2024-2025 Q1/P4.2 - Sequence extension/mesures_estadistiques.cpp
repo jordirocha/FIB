344c01c062fbebe573b596bd69c6147056bbfd47
Jordi Rocha <jordirocharocha@gmail.com>
2024-10-23 10:53:49 +0200

    more files

---
+++ Jutge/PRO1 2024-2025 Q1/P4.2 - Sequence extension/mesures_estadistiques.cpp
@@ -0,0 +1,26 @@
+
+
+    #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout.setf(ios::fixed);
+    cout.precision(4);
+int a;
+double min, max, avg;
+
+cin >> x;
+
+for(int i = 0; i < x; i++){
+
+int b;
+cin >> b;
+
+    for(int j = 0;j < b; j++){
+    
+}
+}
+    
+}

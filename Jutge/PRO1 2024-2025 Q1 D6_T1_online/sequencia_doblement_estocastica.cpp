375d3720bd477bc282a45e5d9cf14b50ec0eabc6
Jordi Rocha <jordirocharocha@gmail.com>
2024-12-03 21:08:02 +0100

    new files

--- Jutge/PRO1 2024-2025 Q1 D6_T1_online/sequencia_doblement_estocastica.cpp
+++ Jutge/PRO1 2024-2025 Q1 D6_T1_online/sequencia_doblement_estocastica.cpp
@@ -2,43 +2,41 @@
 // Created by jordi on 27/11/24.
 //
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
 
-typedef vector<int> Counters;
-
+typedef vector<int> Data;
+// Para este problema, mi solución consiste en leer los valores de entrada como vectores de tamaño n,
+// entonces durante la lectura de un fila compruebo si cumple si es estocastico y para las columnas almaceno la suma
+// en un vector de tamaño n, comprobando si alfinal si es estocastico
 int main() {
-    int r;
-
-
-
-    while (cin >> r) {
+    int n;
+    while (cin >> n) {
+        int size = n;
+        // boleanos de comprobación para la fila y columna
         bool is_stochastic_h = true, is_stochastic_v = true;
-        Counters c(r);
-        Counters c_v(r);
+        // suma_v es el vector donde almacenamos la suma de cada columan de la "matriz"
+        Data suma_v(size);
 
-        for (int i = 0; i < r; i++) {
+        for (int i = 0; i < size; i++) {
+            // la variable suma, almacena la suma total de una fila
             int suma = 0;
-            for (int j = 0; j < r; j++) {
+            for (int j = 0; j < size; j++) {
                 int x;
                 cin >> x;
                 suma += x;
-                c[j] += x;
+                // almaceno y acumulo para cada posicion el valor de cada columna
+                suma_v[j] += x;
             }
-            c_v[i] = suma;
+            // compruebo si para la fila leida cumple con la condicion, en el caso de que no ya puedo
+            // obtener saber que no lo es porque dejara la variable en falso
+            if (is_stochastic_h) is_stochastic_h = (suma == 10);
         }
 
-        for (int i = 0; i < r and is_stochastic_h; i++) {
-            if (c_v[i] != 10) {
-                is_stochastic_h = false;
-            }
-        }
-
-        for (int i = 0; i < r and is_stochastic_v; i++) {
-            if (c[i] != 10) {
-                is_stochastic_v = false;
-            }
+        // compruebo si las sumas de las columnas cumplen con la condicion, a la minima que una no lo comple termino
+        // el bucle
+        for (int i = 0; i < size and is_stochastic_v; i++) {
+            if (suma_v[i] != 10) is_stochastic_v = false;
         }
 
         if (is_stochastic_h and is_stochastic_v) {
@@ -47,7 +45,5 @@ int main() {
             cout << "false" << endl;
         }
     }
-
-
     return 0;
 }

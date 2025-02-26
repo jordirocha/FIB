5fa2ab146869729dfbef709769423367b3ece617
jordi_rocha <jordirocharocha@gmail.com>
2025-02-26 20:53:17 +0100

    New files added

--- Jutge/PRO1 - 2025 - Primavera/Funcions/seguent_primer.cpp
+++ Jutge/PRO1 - 2025 - Primavera/Funcions/seguent_primer.cpp
@@ -1,19 +1,31 @@
+//
+// El programa lee un número primo y obtiene el siguiente, terminamos al introducir uno no primo
+// Input: un número natural primo
+// Output: el siguiente número primo
+//
 #include <iostream>
 
 using namespace std;
 
+//
+// Mira si un número es primo, sí o no
+// Pre: número natural
+// Post: devuelve cierto si lo es, en caso contrario falso
+//
 bool es_primo(int x)
 {
     if (x < 2)
         return false;
     for (int d = 2; d * d <= x; ++d)
-    {
         if (x % d == 0)
             return false;
-    }
     return true;
 }
-
+//
+// Obtiene el siguiente número primo
+// Pre: número natural
+// Post: devuelve el siguiente número primo
+//
 int siguiente_primo(int x)
 {
     int num = x + 1;

f6edabf2cf6d6b9b2326e7dcb7e8b470ca78035b
jordi_rocha <jordirocharocha@gmail.com>
2025-02-27 14:04:33 +0100

    New files added

--- Jutge/PRO1 - 2025 - Primavera/Funcions/factor_mes_frequent.cpp
+++ Jutge/PRO1 - 2025 - Primavera/Funcions/factor_mes_frequent.cpp
@@ -7,115 +7,70 @@ using namespace std;
 
 void factorizar(int n, int &f, int &q)
 {
+    // int f = -1, q = 1000;
+    q = -1;
     int d = 2;
     int currentFactor = 0;
+    bool firstFactor = false;
     int counter = 0;
     while (n != 1)
     {
+        bool isFactor = false;
         if (n % d == 0)
         {
-            cout << "factor: " << d << endl;
+            // cout << "factor: " << d << endl;
             currentFactor = d;
+            isFactor = true;
+            // firstFactor = true;
             counter++;
             n /= d;
         }
         else
         {
+            // cout << "no factor: " << d << endl;
             // if (counter > 0)
             // {
             //     // cout << currentFactor << " " << counter << endl;
             //     cout << counter << endl;
             // }
             ++d;
-            counter = 0;
-        }
-
-        if (currentFactor == d)
-        {
-            cout << " puedo seguir diviendo entre: " << currentFactor << " " << counter << endl;
         }
 
         // if (currentFactor != d)
         // {
-        //     cout << currentFactor << " " << counter << endl;
+        // cout << currentFactor;
+        // cout << "diferente a " << d << endl;
+        if (counter > 0)
+        {
+            if (not firstFactor)
+            {
+                f = currentFactor;
+                q = counter;
+                firstFactor = true;
+            }
+
+            // cout << "entro aqui porque el factor " << currentFactor << endl;
+            // cout << "esta elevad a  " << counter << endl;
+            // cout << "y el siguiente factor es  " << d << endl;
+            // if (currentFactor > f and counter > q)
+            if (currentFactor <= f)
+            {
+                f = currentFactor;
+                q += counter;
+
+                cout << f << " " << q << endl;
+            }
+            counter = 0;
+        }
+
         // }
+        // else
         // {
-        //     counter = 0;
+        // cout << currentFactor;
+        // cout << "igual a " << d << endl;
         // }
     }
 }
-
-// int d = 2;
-// bool firstFactor = false;
-// int potencia = 0;
-// while (n != 1)
-// {
-//     // int divisor = i;
-//
-//     if (n % d == 0)
-//     {
-//         cout << d << endl;
-//         potencia++;
-//         n = n / d;
-//     }
-//     else
-//     {
-//         ++d;
-//     }
-//
-//     if (not firstFactor)
-//     {
-//         f = d;
-//         q = potencia;
-//         firstFactor = true;
-//     }
-//
-//     if (potencia > 0)
-//     {
-//         cout << "potencias: " << potencia << endl;
-//         if (potencia > q)
-//
-//         {
-//             // cout <<  << potencia << endl;
-//             f = d;
-//             q = potencia;
-//         }
-//     }
-// }
-// }
-
-// void factor(int n, int &f, int &q)
-// {
-//     bool firstFactor = false;
-//
-//     for (int i = 2; n > 1; i++)
-//     {
-//         int divisor = i;
-//         int potencia = 0;
-//         while (n % i == 0)
-//         {
-//             // cout << i << endl;
-//             potencia++;
-//             n /= i;
-//         }
-//
-//         if (potencia > 0)
-//         {
-//             // cout << "resultado: " << divisor << " ^ " << potencia << endl;
-//             if (not firstFactor)
-//             {
-//                 f = divisor;
-//                 q = potencia;
-//                 firstFactor = true;
-//             }
-//             if (potencia > q)
-//             {
-//                 f = divisor;
-//                 q = potencia;
-//             }
-//         }
-//     }
-// }
 
 int main()
 {

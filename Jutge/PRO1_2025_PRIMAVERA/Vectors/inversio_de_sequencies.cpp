17465dc442d77bcfb4a909c2ba26e2f721b0e849
jordi_rocha <jordirocharocha@gmail.com>
2025-03-03 16:03:18 +0100

    New files added

--- Jutge/PRO1_2025_PRIMAVERA/Vectors/inversio_de_sequencies.cpp
+++ Jutge/PRO1_2025_PRIMAVERA/Vectors/inversio_de_sequencies.cpp
@@ -1,12 +1,31 @@
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-int a,b,c;
-cin>>a>>b>>c;
-cout<<a+b+c<<endl;
+int main()
+{
+    int a;
+    while (cin >> a)
+    {
+        vector<int> elements(a);
+        for (int i = 0; i < a; i++)
+            cin >> elements[i];
+        if (a == 1)
+        {
+            cout << elements[0] << endl;
+        }
+        else
+        {
+            for (int i = elements.size() - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                    cout << elements[i];
+                else
+                    cout << elements[i] << " ";
+            }
+            cout << endl;
+        }
+    }
 }
-
-

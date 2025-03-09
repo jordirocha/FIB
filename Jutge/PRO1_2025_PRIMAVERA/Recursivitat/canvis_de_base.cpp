36831e5ae41b492fce6426265df947c2eadaf010
jordi_rocha <jordirocharocha@gmail.com>
2025-03-09 13:37:51 +0100

    New files added

--- Jutge/PRO1_2025_PRIMAVERA/Recursivitat/canvis_de_base.cpp
+++ Jutge/PRO1_2025_PRIMAVERA/Recursivitat/canvis_de_base.cpp
@@ -3,18 +3,52 @@
 
 using namespace std;
 
-int toBinary(int x)
+// void toBinary(int x)
+// {
+//
+//     if (x == 1)
+//     {
+//         cout << 1;
+// //        return 1;
+//     }
+//
+//
+// //    int cociente = toBinary(x / 2);
+//     toBinary(x / 2);
+//     // cout << cociente << endl;
+// //    return cociente;
+// }
+
+void toBinary(int x)
 {
+    int residuo;
     if (x == 1)
     {
-        return 1;
+        // residuo = 1;
+        cout << "1";
+    }
+    else if (x == 2)
+    {
+        cout << "0";
+        cout << "1";
+        // residuo = 1;
+    }
+    else if (x == 3)
+    {
+        cout << "1";
+        cout << "1";
+    }
+    else
+    {
+        cout << (x % 2) << endl;
+        toBinary(x / 2);
     }
 
-    cout << x % 2 << endl;
-    int cociente = toBinary(x / 2);
-
+    //    int cociente = toBinary(x / 2);
     // cout << cociente << endl;
-    return cociente;
+    //    return cociente;
+
+    // return residuo;
 }
 
 int main()
@@ -22,6 +56,11 @@ int main()
     int a;
     while (cin >> a)
     {
-        cout << toBinary(a) << endl;
+        toBinary(a);
+        // while (a > 0)
+        // {
+        //     cout << a % 2 << endl;
+        //     a = a / 2;
+        // }
     }
 }

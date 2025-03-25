ba233c645a620404ea9e0a3eea49a877e1c38ac7
Jordi Rocha <alan.jordi.rocha@estudiantat.upc.edu>
2025-03-25 15:12:13 +0100

    new changes

--- EXAMENS_DE_P1/CONTROLS_P1/control_c202_c.cpp
+++ EXAMENS_DE_P1/CONTROLS_P1/control_c202_c.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 int main()
 {
-    int n, a = 0, b = 0, c = 0;
-    char maxCh = 'a';
+    int n, a = 0, b = 0, c = 0, max = 0;
+    char l = '-';
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         char ch;
         cin >> ch;
-        if (ch == 'a')
-            a++;
-        else if (ch == 'b')
-            b++;
-        else
-            c++;
+        if (ch == 'a') { a++; }
+        else if (ch == 'b') { b++; }
+        else { c++; }
     }
-    int max = a;
+    if (a > max)
+    {
+        l   = 'a';
+        max = a;
+    }
     if (b > max)
     {
-        maxCh = 'b';
-        max   = b;
+        l   = 'b';
+        max = b;
     }
-    else if (c > max)
+    if (c > max)
     {
-        maxCh = 'c';
-        max   = c;
+        l   = 'c';
+        max = c;
     }
-
-    cout << "majoria de " << maxCh << endl << max << " repeticio(ns)" << endl;
+    cout << "majoria de " << l << endl << max << " repeticio(ns)" << endl;
 }

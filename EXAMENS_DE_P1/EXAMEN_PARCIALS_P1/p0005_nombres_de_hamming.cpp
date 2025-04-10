9eb13758553e04f5bc05e05ccc747f6ffca457c3
Jordi Rocha <alan.jordi.rocha@estudiantat.upc.edu>
2025-04-10 12:49:51 +0200

    New files added

--- EXAMENS_DE_P1/EXAMEN_PARCIALS_P1/p0005_nombres_de_hamming.cpp
+++ EXAMENS_DE_P1/EXAMEN_PARCIALS_P1/p0005_nombres_de_hamming.cpp
@@ -3,66 +3,35 @@ using namespace std;
 
 bool es_de_hamming(int x)
 {
-    // int d    = 2;
-    bool ham = true;
-    while (x % 2 == 0)
-    {
-        // cout << 2 << endl;
-        x /= 2;
-    }
-    for (int i = 3; i * i <= x and ham; i += 2)
-    {
-        bool isValid = true;
-        // if (x % i == 0)
-        // {
-        //     if (i == 3 or i == 5)
-        //     {
-        while (isValid and x % i == 0)
-        {
-            // cout << i << endl;
-            if (i == 3 or i == 5) { x /= i; }
-            else
-            {
-                ham     = false;
-                isValid = false;
-            }
-        }
-        //     }
-        //     else { ham = false; }
-        // }
-    }
-    // if (x > 1) cout << x << endl;
-    if (ham and x > 1) { ham = (x == 3 or x == 5); }
-    // while (ham and d * d <= x)
-    // {
-    //     if (x % d == 0)
-    //     {
-    //         if (d == 2 or d == 3 or d == 5)
-    //             while (x % d == 0) { x /= d; }
-    //         else { ham = false; }
-    //     }
-    //     d++;
-    // }
-    // if (ham and x > 1) { ham = (x == 2 or x == 3 or x == 5); }
-    return ham;
+    while (x % 2 == 0) x /= 2;
+    while (x % 3 == 0) x /= 3;
+    while (x % 5 == 0) x /= 5;
+    return x == 1;
 }
 
 int main()
 {
-    int a;
-    // cout << es_de_hamming(120) << endl;
-    while (cin >> a)
+    int n;
+    while (cin >> n)
     {
-        int i = 1, num = 2;
-        if (a != 0) cout << 1;
-        while (i < a)
+        int number = 1;
+        if (n > 0)
         {
-            if (es_de_hamming(num))
+            if (es_de_hamming(number))
             {
-                cout << ',' << num;
-                i++;
+                cout << number;
+                --n;
             }
-            num++;
+        }
+        ++number;
+        while (n > 0)
+        {
+            if (es_de_hamming(number))
+            {
+                cout << ',' << number;
+                --n;
+            }
+            ++number;
         }
         cout << endl;
     }
--- PRO1_2025_PRIMAVERA/SEQUENCIES_3/tobogans.cpp
+++ PRO1_2025_PRIMAVERA/SEQUENCIES_3/tobogans.cpp
@@ -1,112 +1,55 @@
 #include <iostream>
 using namespace std;
 
+void changeData(int count, int i, int slope, int &newPos, int &newSlope,
+                int &newLong)
+{
+    newPos   = i - count;
+    newSlope = slope;
+    newLong  = count;
+}
+
+void data(int count, int i, int slope, int &posAsc, int &penAsc, int &longAsc,
+          int &posDesc, int &penDesc, int &longDesc)
+{
+    if (count >= 3)
+    {
+        if (slope > 0 and count > longAsc)
+            changeData(count, i, slope, posAsc, penAsc, longAsc);
+        else if (slope < 0 and count > longDesc)
+            changeData(count, i, slope, posDesc, penDesc, longDesc);
+    }
+}
+
 int main()
 {
-    int x, y, currentPend = 0, count = 2, currentPos = 2;
-    // ascendentes
-    int posAscending = 0, pendienteAscending = 0, longAscending = 0;
-    // descendentes
-    int posDescending = 0, pendienteDescending = 0, longDescending = 0;
+    int x, y, count = 2, i = 2, posAsc = 0, pendAsc = 0, longAsc = 0,
+              posDesc = 0, pendDesc = 0, longDesc = 0;
     cin >> x >> y;
-    currentPend = y - x;
-    x           = y;
-
+    int slope = y - x;
+    x         = y;
     while (cin >> y)
     {
-        currentPos++;
-        // cout << "numeros a comparar: " << x << " " << y << endl;
+        i++;
         int diff = y - x;
         if (diff != 0)
         {
-            if (diff == currentPend)
-            {
-                cout << x << " " << y << " <- misma pendiente " << endl;
-                count++;
-                currentPend = diff;
-            }
+            if (diff == slope) { count++; }
             else
             {
-                // cout << "total de numeros con misma pendiente: " << count
-                //      << endl;
-
-                if (count >= 3)
-                {
-                    cout << "entro a qui por que hay otra secuencia distint"
-                         << x << " " << y << " <- distinta pendiente " << endl;
-
-                    if (currentPend > 0)
-                    {
-                        if (count > longAscending)
-                        {
-                            posAscending       = currentPos - count;
-                            pendienteAscending = currentPend;
-                            longAscending      = count;
-                            // cout << "/ " << posAscending << ':'
-                            //      << pendienteAscending << ':' <<
-                            //      longAscending
-                            //      << endl;
-                        }
-                    }
-                    else if (currentPend < 0)
-                    {
-                        if (count > longDescending)
-                        {
-                            posDescending       = currentPos - count;
-                            pendienteDescending = currentPend;
-                            longDescending      = count;
-                            cout << "\\ " << posDescending << ':'
-                                 << pendienteDescending << ':' << longDescending
-                                 << endl;
-                        }
-                    }
-                    // cout << "pendientes anterior: " << currentPend << endl;
-                    // cout << "pendiente nueva: " << diff << endl;
-                }
-
-                count       = 2;
-                currentPend = diff;
+                data(count, i, slope, posAsc, pendAsc, longAsc, posDesc,
+                     pendDesc, longDesc);
+                count = 2;
             }
+            slope = diff;
         }
         x = y;
     }
-    currentPos++;
-    cout << "ultina posicion " << currentPos << endl;
-
-    if (count >= 3)
-    {
-        // cout << "falta comprobar para la salida, porque hay un tobogan de "
-        //      << count << endl;
-        if (currentPend > 0)
-        {
-            if (count > longAscending)
-            {
-                posAscending       = currentPos - count;
-                pendienteAscending = currentPend;
-                longAscending      = count;
-                // cout << "/ " << posAscending << ':' << pendienteAscending <<
-                // ':'
-                //      << longAscending << endl;
-            }
-        }
-        else if (currentPend < 0)
-        {
-            if (count > longDescending)
-            {
-                posDescending       = currentPos - count;
-                pendienteDescending = currentPend;
-                longDescending      = count;
-                // cout << "\\ " << posDescending << ':'
-                //      << pendienteDescending << ':' << longDescending
-                //      << endl;
-            }
-        }
-    }
-
-    if (longAscending != 0)
-        cout << "/ " << posAscending << ':' << pendienteAscending << ':'
-             << longAscending << endl;
-    if (longDescending != 0)
-        cout << "\\ " << posDescending << ':' << pendienteDescending << ':'
-             << longDescending << endl;
+    i++;
+    data(count, i, slope, posAsc, pendAsc, longAsc, posDesc, pendDesc,
+         longDesc);
+    if (longAsc != 0)
+        cout << "/ " << posAsc << ':' << pendAsc << ':' << longAsc << endl;
+    if (longDesc != 0)
+        cout << "\\ " << posDesc << ':' << pendDesc << ':' << longDesc << endl;
 }

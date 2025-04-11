a42b8fa7c95634640a0d4e218f4cb3e986d7120e
jordi_rocha <jordirocharocha@gmail.com>
2025-04-11 13:43:41 +0200

    New files added

--- PRO1_2025_PRIMAVERA/Vectors/longitud_mitjana_i_lletra_mes_frequent.cpp
+++ PRO1_2025_PRIMAVERA/Vectors/longitud_mitjana_i_lletra_mes_frequent.cpp
@@ -1,12 +1,46 @@
-
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
-int main() {
-int a,b,c;
-cin>>a>>b>>c;
-cout<<a+b+c<<endl;
+char lletra_mes_frequent(const string &s)
+{
+    char ch = 'z';
+    int max = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        int c = 0;
+        for (int j = 0; j < s.length(); j++)
+            if (s[i] == s[j]) c++;
+        if (c == max)
+        {
+            if (s[i] < ch) ch = s[i];
+        }
+        else if (c > max)
+        {
+            max = c;
+            ch  = s[i];
+        }
+    }
+    return ch;
 }
-
-
+int main()
+{
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    int n;
+    while (cin >> n)
+    {
+        vector<string> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < v[i].length(); j++) sum += 1;
+        }
+        cout << (sum / n) << endl;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i].length() >= (sum / n))
+                cout << v[i] << ": " << lletra_mes_frequent(v[i]) << endl;
+        }
+    }
+}
